778c8c38cc86a78034927e4182bc0d75976b56d3
hadi <hadi.lok8004@gmail.com>
2021-05-03 17:43:04 +0430

    check7

--- pyServer/hotMoney.py
+++ pyServer/hotMoney.py
@@ -819,12 +819,12 @@ logging.basicConfig(filename="log.txt",
 # logging.exception('exception')
 logger = logging.getLogger('urbanGUI')
 
-schedule.every().saturday.at("08:55").do(startServer)
-schedule.every().sunday.at("08:55").do(startServer)
-schedule.every().monday.at("08:55").do(startServer)
-schedule.every().tuesday.at("08:55").do(startServer)
-schedule.every().wednesday.at("08:55").do(startServer)
-schedule.every().day.at("18:00").do(downloadCsvs)
+schedule.every().saturday.at("09:00").do(startServer)
+schedule.every().sunday.at("09:00").do(startServer)
+schedule.every().monday.at("09:00").do(startServer)
+schedule.every().tuesday.at("09:00").do(startServer)
+schedule.every().wednesday.at("09:00").do(startServer)
+schedule.every().day.at("09:00").do(downloadCsvs)
 
 while True:
     schedule.run_pending()
