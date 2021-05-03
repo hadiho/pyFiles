0927895b2cf8a730d6cd7309052ffa176ccea8ff
hadi <hadi.lok8004@gmail.com>
2021-05-03 16:21:27 +0430

    check3

--- pyServer/hotMoney.py
+++ pyServer/hotMoney.py
@@ -824,7 +824,7 @@ schedule.every().sunday.at("08:55").do(startServer)
 schedule.every().monday.at("08:55").do(startServer)
 schedule.every().tuesday.at("08:55").do(startServer)
 schedule.every().wednesday.at("08:55").do(startServer)
-schedule.every().day.at("15:57").do(downloadCsvs)
+schedule.every().day.at("16:30").do(downloadCsvs)
 
 while True:
     schedule.run_pending()
