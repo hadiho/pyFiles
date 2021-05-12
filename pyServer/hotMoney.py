27b6a46988a4a07adfb276ead417936dc11c6f2d
hadi <hadi.lok8004@gmail.com>
2021-05-12 16:48:13 +0430

    download 4

--- pyServer/hotMoney.py
+++ pyServer/hotMoney.py
@@ -806,7 +806,7 @@ def downloadCsvs():
         df.to_csv('client_types_data/' + symbol + '.csv', index=False)
         print(symbol)
     print("finish download csv")
-    timeVolume()
+    # timeVolume()
 
 def downloadOneCsv(symbol):
     print("to download Csv ...")
@@ -828,21 +828,21 @@ logging.basicConfig(filename="log.txt",
 
 logger = logging.getLogger('urbanGUI')
 
-schedule.every().saturday.at("09:00").do(startServer)
-schedule.every().sunday.at("09:00").do(startServer)
-schedule.every().monday.at("09:00").do(startServer)
-schedule.every().tuesday.at("10:07").do(startServer)
-schedule.every().wednesday.at("09:00").do(startServer)
-# schedule.every().day.at("17:00").do(downloadCsvs)
-
-while True:
-    schedule.run_pending()
-    time.sleep(5)
+# schedule.every().saturday.at("09:00").do(startServer)
+# schedule.every().sunday.at("09:00").do(startServer)
+# schedule.every().monday.at("09:00").do(startServer)
+# schedule.every().tuesday.at("10:07").do(startServer)
+# schedule.every().wednesday.at("09:00").do(startServer)
+# # schedule.every().day.at("17:00").do(downloadCsvs)
+#
+# while True:
+#     schedule.run_pending()
+#     time.sleep(5)
 
 
 # downloadOneCsv('فایرا')
 # startServer()
-# downloadCsvs()
+downloadCsvs()
 # detectVolume()
 # timeVolume()
 # all_stocks()
